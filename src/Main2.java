2af41f9a6a4268dd0a79f09e1ca8eea447c5b821
userNameFl <flissek.@gmail.com>
2017-12-10 15:57:28 +0100

    commit intellij2

--- src/Main2.java
+++ src/Main2.java
@@ -5,6 +5,7 @@ public class Main2 {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("new line");
+        System.out.println("next");
 
     }
 }
